983e00fcd4973bcb27768d8d89a466d3e7310d4e
Luis Hernández <luishernandez.mail@gmail.com>
2015-09-04 12:40:24 +0200

    Finished Event CRUD test

--- tests/codeception/acceptance/EventCept.php
+++ tests/codeception/acceptance/EventCept.php
@@ -6,3 +6,27 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('Ensure Event CRUD works');
 $I->amOnPage(Yii::$app->getUrlManager()->createUrl(['event/index']));
 $I->see('Create Event');
+$I->see('Salsea');
+$I->see('Bachatea');
+
+$I->click('a[title="View"]');
+$I->expectTo('See details of first Event');
+$I->see('Salsea');
+$I->see('Update');
+
+$I->click('Update');
+$I->expectTo('See an Event Editor page');
+$I->seeInField('Name', 'Salsea');
+$I->fillField('Name', 'Salsatorium');
+$I->click('Update');
+$I->expectTo('See updated name for the Event');
+$I->waitForText('Delete'); // Wait for page change after JS validation of fields
+$I->see('Salsatorium');
+$I->see('Delete');
+
+$I->click('Delete');
+$I->acceptPopup();
+$I->expect('Event to be gone');
+$I->see('Create Event');
+$I->see('Bachatea');
+$I->dontSee('Salsea');
