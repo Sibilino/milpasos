8ee416ac9b6120dbf2d6a19b43fff8b406624725
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-07 15:30:46 +0100

    Added DanceFixture

---
+++ tests/codeception/fixtures/DanceFixture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace tests\codeception\fixtures;
+
+use yii\test\ActiveFixture;
+
+/**
+ * Dance fixture
+ */
+class DanceFixture extends ActiveFixture
+{
+    public $modelClass = 'app\models\Dance';
+}
