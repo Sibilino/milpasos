883c8974038e249bd77ee98fa82d44c3af378606
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-07 21:10:54 +0100

    Troubleshooting

--- components/ManyToManyBehavior.php
+++ components/ManyToManyBehavior.php
@@ -7,13 +7,28 @@ use yii\base\Event;
 use yii\base\Model;
 use yii\db\ActiveRecord;
 use yii\db\AfterSaveEvent;
+use yii\helpers\ArrayHelper;
 use yii\validators\EachValidator;
 
+/**
+ * Class ManyToManyBehavior
+ * @property ActiveRecord $owner
+ * @package app\components
+ */
 class ManyToManyBehavior extends Behavior
 {
+    /**
+     * @var string
+     */
     public $relation;
+    /**
+     * @var string
+     */
     public $idListAttr;
-    
+
+    /**
+     * @return array
+     */
     public function events() {
         return [
             Model::EVENT_AFTER_VALIDATE => 'validateIdList',
@@ -22,7 +37,10 @@ class ManyToManyBehavior extends Behavior
             ActiveRecord::EVENT_AFTER_UPDATE => 'saveRelation',
         ];
     }
-    
+
+    /**
+     * @param Event $event
+     */
     public function validateIdList(Event $event) {
         $relationClass = $this->owner->getRelation($this->relation)->modelClass;
         // TODO: Replace each + exist validators for one findAll()
@@ -31,16 +49,22 @@ class ManyToManyBehavior extends Behavior
         ]);
         $validator->validateAttribute($this->owner, $this->idListAttr);
     }
-    
+
+    /**
+     * @param Event $event
+     */
     public function loadIdList(Event $event) {
         $model = $this->owner;
-        $model->${$this->idListAttr} = ArrayHelper::getColumn($model->${$this->relation}, 'id');
+        $model->{$this->idListAttr} = ArrayHelper::getColumn($model->{$this->relation}, 'id');
     }
-    
+
+    /**
+     * @param AfterSaveEvent $event
+     */
     public function saveRelation(AfterSaveEvent $event) {
         $model = $this->owner;
         $model->unlinkAll($this->relation, true); // Delete all existing links
-        $ids = $model->${$this->idListAttr};
+        $ids = $model->{$this->idListAttr};
         $relatedModels = $model->getRelation($this->relation)->andWhere(['id' => $ids])->all();
         
         foreach ($relatedModels as $record) {
