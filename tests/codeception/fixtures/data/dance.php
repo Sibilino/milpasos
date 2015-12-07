a7a2500f0887291081ea7e8697759b693bae927b
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-07 15:32:35 +0100

    Added Dance fixture data

---
+++ tests/codeception/fixtures/data/dance.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    [
+        'id' => '1',
+        'name' => 'Bachata',
+    ],
+    [
+        'id' => '2',
+        'name' => 'Kizomba',
+    ],
+    [
+        'id' => '3',
+        'name' => 'Salsa',
+    ],
+];
