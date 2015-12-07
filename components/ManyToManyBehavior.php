52c8ea19c81c1e6159b97d5586b68ff893435398
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-07 12:27:59 +0100

    First implementation of ManyToManyBehavior

--- components/ManyToManyBehavior.php
+++ components/ManyToManyBehavior.php
@@ -9,6 +9,10 @@ use yii\db\AfterSaveEvent;
 
 class ManyToManyBehavior extends Behavior
 {
+    public $relation;
+    
+    public $idListAttr;
+    
     public function events() {
         return [
             ActiveRecord::EVENT_AFTER_FIND => 'loadIdList',
@@ -16,10 +20,22 @@ class ManyToManyBehavior extends Behavior
             ActiveRecord::EVENT_AFTER_UPDATE => 'saveRelation',
         ];
     }
+    
     public function loadIdList(Event $event) {
-        // TODO
+        $model = $this->owner;
+        $model->${$this->idListAttr} = ArrayHelper::getColumn($model->${$this->relation}, 'id');
     }
+    
     public function saveRelation(AfterSaveEvent $event) {
-        // TODO
+        $model = $this->owner;
+        $relationFinder = $model->getRelation($this->relation);
+        
+        $model->unlinkAll($this->relation, true); // Delete all existing links
+        foreach ($model->${$this->idListAttr} as $id) {
+            $relatedRecord = $relationFinder->where(['id' => $id])->one();
+            if ($relatedRecord !== null) {
+                $this->link($this->relation, $relatedRecord);
+            }
+        }
     }
 }
