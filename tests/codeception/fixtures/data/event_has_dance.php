1744a91e4e61aa0db010bc93158572260be21581
Luis Hernández <luishernandez.mail@gmail.com>
2015-12-09 12:59:06 +0100

    Create event_has_dance fixture data

---
+++ tests/codeception/fixtures/data/event_has_dance.php
@@ -0,0 +1,19 @@
+<?php
+return [
+  [
+    'event_id' => 1,
+    'dance_id' => 2,
+  ],
+  [
+    'event_id' => 1,
+    'dance_id' => 3,
+  ],
+  [
+    'event_id' => 2,
+    'dance_id' => 1,
+  ],
+  [
+    'event_id' => 2,
+    'dance_id' => 2,
+  ],
+];
