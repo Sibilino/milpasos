19cff0fa837983770c8c055b6000ed702355fab5
Luis Hernández <luishernandez.mail@gmail.com>
2016-01-13 15:42:59 +0100

    Event_ids in Pass must now exist as Event

--- models/Pass.php
+++ models/Pass.php
@@ -44,13 +44,13 @@ class Pass extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            ['full', 'event_id'], 'required'],
+            ['full', 'event_id', 'price', 'currency'], 'required'],
             [['price'], 'number', 'min' => 0],
             [['full'], 'boolean'],
             [['available_from', 'available_to', 'price'], 'default', 'value' => null],
             [['available_from', 'available_to'], 'date', 'format' => 'yyyy-MM-dd'],
             [['currency'], 'in', 'range' => array_keys(static::$currencies), 'strict' => true],
-            [['event_id'], 'integer'],
+            [['event_id'], 'exist', 'targetClass' => Event::className(), 'targetAttribute' => 'id'],
             [['description'], 'string', 'max' => 1000]
         ];
     }
