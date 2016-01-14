22218a5503491f2bb39bb95126506011186148d7
Luis Hernández <luishernandez.mail@gmail.com>
2016-01-14 12:06:43 +0100

    Added $newTemporaryPrice to pass update view

--- views/pass/update.php
+++ views/pass/update.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Pass */
+/* @var $newTemporaryPrice app\models\TemporaryPrice */
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'Pass',
@@ -18,6 +19,7 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
     <?= $this->render('_form', [
         'model' => $model,
+        'newTemporaryPrice' => $newTemporaryPrice,
     ]) ?>
 
 </div>
