ca72788854cf31a6ea7440c7301cfbe1c678e747
Luis Hernández <luishernandez.mail@gmail.com>
2016-01-14 12:23:03 +0100

    Added tempPrice GridForm to pass/update

--- views/pass/_form.php
+++ views/pass/_form.php
@@ -2,21 +2,26 @@
 
 use app\models\Event;
 use app\models\Pass;
+use app\models\TemporaryPrice;
 use app\widgets\DateRangePicker;
+use app\widgets\GridForm;
+use yii\data\ActiveDataProvider;
+use yii\grid\ActionColumn;
+use yii\grid\DataColumn;
+use yii\grid\SerialColumn;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Pass */
+/* @var $newTemporaryPrice app\models\TemporaryPrice */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
 <div class="pass-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
@@ -40,5 +45,51 @@ use yii\helpers\ArrayHelper;
     </div>
 
     <?php ActiveForm::end(); ?>
+    
+    <?php if (!$model->isNewRecord && isset($newTemporaryPrice)): ?>
+        <div id="temporary-prices">
+            <h2><?= Yii::t('app', "Temporary Prices") ?></h2>
+            
+            <?php $form = GridForm::begin([
+                'gridOptions' => [
+                    'dataProvider' => new ActiveDataProvider([
+                        'query' => $model->getTemporaryPrices(),
+                    ]),
+                    "columns" => [
+                        [
+                            'class' => SerialColumn::className(),
+                        ],
+                        [
+                            'attribute' => 'price',
+                            'value' => function (TemporaryPrice $tempPrice, $key, $index, DataColumn $column) use ($model) {
+                                return $column->grid->formatter->asCurrency($tempPrice->price, $model->currency);
+                            },
+                        ],
+                        'available_from:date',
+                        'available_to:date',
+                        [
+                            'class' => ActionColumn::className(),
+                            'header' => 'actions',
+                            'template' => '{update}{delete}',
+                            'controller' => 'temporary-price',
+                        ],
+                    ],
+                ],
+            ]); ?>
+
+            <?= $form->field($newTemporaryPrice, 'price')->textInput(['maxlength' => true]) ?>
+            <?= DateRangePicker::widget([
+                'form' => $form,
+                'model' => $newTemporaryPrice,
+                'fromAttr' => 'available_from',
+                'toAttr' => 'available_to',
+            ]) ?>
+            
+            <?= Html::submitButton("Add", ['class' => 'btn btn-danger']) ?>
+
+            <?php GridForm::end() ?>
+        </div>
+    
+    <?php endif; ?>
 
 </div>
