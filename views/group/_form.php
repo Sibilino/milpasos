cdee65f4f8e8a359bdafdbf0812ffbe49d4f03fb
Luis Hernández <luishernandez.mail@gmail.com>
2016-02-16 15:21:34 +0100

    Added image to Group form

--- views/group/_form.php
+++ views/group/_form.php
@@ -13,6 +13,10 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?php if ($model->imageUrl): ?>
+        <img src="<?= $model->imageUrl ?>">
+    <?php endif; ?>
+    <?= $form->field($model, 'imageUrl')->fileInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
