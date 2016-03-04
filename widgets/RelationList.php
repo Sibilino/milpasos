5fbda3267aa17a9cb4d44f2dd54c47a8bd1c3794
Luis Hernández <luishernandez.mail@gmail.com>
2016-03-04 12:51:39 +0100

    Commented RelationList

--- widgets/RelationList.php
+++ widgets/RelationList.php
@@ -5,21 +5,46 @@ namespace app\widgets;
 use ReflectionClass;
 use Yii;
 use yii\base\InvalidConfigException;
+use yii\base\Model;
 use yii\base\Widget;
+use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
 use yii\helpers\Html;
 use yii\helpers\Inflector;
 use yii\helpers\Url;
 
+/**
+ * This widget generates an unordered list of links to each of the records existing for a model's relation.
+ * @package app\widgets
+ */
 class RelationList extends Widget
 {
+    /**
+     * @var Model The model for which to find the related objects.
+     */
     public $model;
+    /**
+     * @var string The name of the relation.
+     */
     public $relation;
-    public $controller;
+    /**
+     * @var string Optional. The name of the controller to which the generated links will point.
+     */
+    public $controller = '';
+    /**
+     * @var string The attribute of the relation that will be used as text for its generated link.
+     */
     public $labelAttr = 'name';
-    
+
+    /**
+     * @var ActiveQuery The Query object of the relation.
+     */
     private $_relationQuery;
-    
+
+    /**
+     * @inheritdoc
+     * @throws InvalidConfigException
+     */
     public function init()
     {
         parent::init();
@@ -32,7 +57,11 @@ class RelationList extends Widget
             $this->controller = Inflector::camel2id($reflection->getShortName());
         }
     }
-    
+
+    /**
+     * @inheritdoc
+     * @return string
+     */
     public function run()
     {
         $widget = $this;
