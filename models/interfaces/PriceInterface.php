c3b3d692300d7e67ff2c0a5eec98834cdf1585ef
Luis Hernández <luishernandez.mail@gmail.com>
2016-03-15 15:38:11 +0100

    Added PriceTrait

--- models/interfaces/PriceInterface.php
+++ models/interfaces/PriceInterface.php
@@ -2,7 +2,21 @@
 
 namespace app\models\interfaces;
 
+use 
+
 interface PriceInterface
 {
-    public function formattedPrice();
+    public function getFormattedPrice();
+}
+
+trait PriceTrait
+{
+    /**
+     * Returns the formatted string that represents $this->price, expressed in $this->currency.
+     * @return string
+     */
+    public function getFormattedPrice()
+    {
+        return Yii::$app->formatter->asCurrency($this->price, $this->currency);
+    }
 }
