f0adcc77e7894aa1e79998c857d693dc304c58a7
Luis Hernández <luishernandez.mail@gmail.com>
2016-03-16 11:04:54 +0100

    Implemented MockCurrencyConverter

--- components/MockCurrencyConverter.php
+++ components/MockCurrencyConverter.php
@@ -1,3 +1,30 @@
 <?php
 
 namespace app\components;
+
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+/**
+ * Configure $rates as an array of currency_code => rate_to_EUR and use the toEur() function to convert an amount.
+ */
+class MockCurrencyConverter extends Component
+{
+    /**
+     * Array of currency_code => rate_to_EUR
+     */
+    public $rates = [];
+    
+    /**
+     * Returns the equivalent price in Euros for the given amount in a configured currency.
+     * @param number $amount
+     * @param string $currency
+     */
+    public function toEur($amount, $currency)
+    {
+        if (!isset($rates[$currency])) {
+            throw new InvalidConfigException("Conversion rate for currency '$currency' to EUR is not defined.");
+        }
+        return $amount * $rates[$currency];
+    }
+}
