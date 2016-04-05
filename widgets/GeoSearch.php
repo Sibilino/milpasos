f52176837a4dd382149e3d528e80bb8f7a22fab7
Luis Hernández <luishernandez.mail@gmail.com>
2016-04-05 20:52:59 +0200

    Added GeoSearch widget skeleton

---
+++ widgets/GeoSearch.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\widgets;
+
+use yii\widgets\InputWidget;
+
+class GeoSearch extends InputWidget
+{
+    /**
+     * Initializes the widget.
+     * If you override this method, make sure you call the parent implementation first.
+     */
+    public function init()
+    {
+        
+        parent::init();
+    }
+
+    /**
+     * Executes the widget.
+     * @return string the result of widget execution to be outputted.
+     */
+    public function run()
+    {
+
+    }
+}
