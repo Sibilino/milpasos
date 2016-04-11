63debd71105906973d896d292648bca884c9e9a4
Luis Hernández <luishernandez.mail@gmail.com>
2016-04-11 12:24:24 +0200

    Started implementing GeoSearch

--- widgets/GeoSearch.php
+++ widgets/GeoSearch.php
@@ -2,17 +2,32 @@
 
 namespace app\widgets;
 
+use app\widgets\assets\GeoSearchAsset;
+use yii\base\InvalidConfigException;
+use yii\helpers\Html;
 use yii\widgets\InputWidget;
 
 class GeoSearch extends InputWidget
 {
+    /**
+     * @var string The model attribute that will receive the longitude coordinate.
+     */
+    public $lonAttribute = 'lon';
+    /**
+     * @var string The model attribute that will receive the latitude coordinate.
+     */
+    public $latAttribute = 'lat';
+    
     /**
      * Initializes the widget.
      * If you override this method, make sure you call the parent implementation first.
      */
     public function init()
     {
-        
+        if (!$this->hasModel())
+            throw new InvalidConfigException(self::className()." requires a Model and an Attribute.");
+        if (!in_array($this->lonAttribute, $this->model->attributes()) || !in_array($this->latAttribute, $this->model->attributes()))
+            throw new InvalidConfigException("The Model must have '$this->lonAttribute' and '$this->latAttribute' attributes.");
         parent::init();
     }
 
@@ -22,6 +37,10 @@ class GeoSearch extends InputWidget
      */
     public function run()
     {
-        MapsAsset::register($this->view);
+        GeoSearchAsset::register($this->view);
+        
+        echo Html::activeHiddenInput($this->model, $this->lonAttribute);
+        echo Html::activeHiddenInput($this->model, $this->latAttribute);
+        echo Html::activeTextInput($this->model, $this->attribute);
     }
 }
