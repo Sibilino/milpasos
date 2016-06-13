9cc99dfc23412ee761e06d4e143b53092f5b26d8
Luis Hernández <luishernandez.mail@gmail.com>
2016-06-13 17:23:02 +0200

    Added callback functions to gmaps.js

--- web/js/gmaps.js
+++ web/js/gmaps.js
@@ -1,5 +1,29 @@
 milpasos.gmaps = (function ($) {
-  var pub = {
-  };
-  return pub;
+    var mapLibraryReady_ = false;
+    var mapCallbacks_ = [];
+    var pub = {
+        isActive: true,
+        /**
+         * Calls all functions in mapCallbacks_ and remembers that the maps library is ready.
+         */
+        initCallback: function () {
+            mapLibraryReady_ = true;
+            for (var i=0;i<mapCallbacks_.length;i++) {
+                mapCallbacks_[i]();
+            }
+        },
+        /**
+         * Register a callback function to be executed only after initCallback() has been called.
+         * Useful to register multiple callbacks to be executed when a maps library is loaded.
+         * @param callback
+         */
+        addCallback: function (callback) {
+            mapCallbacks_.push(callback);
+            if (mapLibraryReady_) {
+                callback();
+            }
+        }
+        
+    };
+    return pub;
 })(jQuery);
