7d45394be630eae9d896f44a3de9163c7648ca9b
Luis Hernández <luishernandez.mail@gmail.com>
2016-06-13 16:21:41 +0200

    Main layout now registers milpasos.js

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,8 +8,10 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use app\assets\MilpasosAsset;
 
 AppAsset::register($this);
+MilpasosAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
