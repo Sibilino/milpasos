d062a30fed4827f7d34c5eb8547268a9ccb7d06f
Luis Hernández <luishernandez.mail@gmail.com>
2016-06-30 15:36:39 +0200

    Configured homeUrl

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $params = require(__DIR__ . '/params.php');
 $config = yii\helpers\ArrayHelper::merge([
         'id' => 'basic',
         'basePath' => dirname(__DIR__),
+        'homeUrl' => ['event/map'],
         'bootstrap' => ['log'],
         'components' => [
             'cache' => [
