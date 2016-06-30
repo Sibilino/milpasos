48f1eda14af9e058102f1d64f27d7c3a8f95ee15
Luis Hernández <luishernandez.mail@gmail.com>
2016-06-30 16:52:18 +0200

    Added pass reset link and translations

--- views/site/login.php
+++ views/site/login.php
@@ -4,16 +4,18 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\forms\LoginForm */
 
+use Yii;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = Yii::t('app', "Login");
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p><?= Yii::t('app', "Please fill out the following fields to log in:") ?></p>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
@@ -31,6 +33,10 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $form->field($model, 'rememberMe')->checkbox([
             'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
         ]) ?>
+        
+        <div style="color:#999;margin:1em 0">
+            <?= Yii::t('app', 'If you forgot your password you can <a href="{url}">reset it</a>', ['url' => Url::to(['site/request-password-reset'])]) ?>
+        </div>
 
         <div class="form-group">
             <div class="col-lg-offset-1 col-lg-11">
