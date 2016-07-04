30aa6816992eb913a49f240deb70f9e215222feb
Luis Hernández <luishernandez.mail@gmail.com>
2016-07-04 11:58:01 +0200

    Removed currencies from params

--- config/params.php
+++ config/params.php
@@ -3,12 +3,5 @@
 return [
     'adminEmail' => 'luishernandez.mail@gmail.com',
     'alertEmail' => 'no-reply@milpasos.com',
-    'currencies' => [
-        'EUR' => '€', // Default
-        'CHF' => 'CHF',
-        'GBP' => '£',
-        'HRK' => 'kn',
-        'USD' => '$',
-    ],
     'user.passwordResetTokenExpire' => 3600,
 ];
