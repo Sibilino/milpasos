07cae48163946ebdd25cbf9233198e20855883a5
Luis Hernández <luishernandez.mail@gmail.com>
2016-07-14 14:30:13 +0200

    Started implementing ListForm

--- widgets/ListForm.php
+++ widgets/ListForm.php
@@ -6,5 +6,27 @@ use yii\widgets\ListView;
 
 class ListForm extends ListView
 {
-
+    public $formView;
+    public $openParam = 'open-model';
+    
+    private $_openModelKey;
+    
+    public function init() {
+        parent::init();
+        $this->_openModelKey = Yii::$app->request->getQueryParam($this->openParam);
+    }
+    
+    public function run() {
+        Pjax::begin();
+        parent::run();
+        Pjax::end();
+    }
+    
+    public function renderItems() {
+        return parent::renderItems().$this->renderForm();
+    }
+    
+    public function renderForm() {
+        return ''; //@TODO
+    }
 }
