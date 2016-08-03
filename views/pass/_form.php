62301524d6e3c9f6931209c2b2f8edb47b6a4a9f
Luis Hernández <luishernandez.mail@gmail.com>
2016-08-03 15:47:40 +0200

    Removed "close" link from pass/_form

--- views/pass/_form.php
+++ views/pass/_form.php
@@ -7,18 +7,11 @@ use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Pass */
-/* @var $listWidget app\widgets\ListForm In case this view is rendered inside a ListForm */
 /* @var $form yii\bootstrap\ActiveForm The active form widget within which to render these fields */
 ?>
 
 <div class="pass-form">
 
-    <?php if (isset($listWidget)):?>
-        <li>
-            <?= Html::encode(Html::getAttributeValue($model, 'description')).' - '.Html::a(Yii::t('app', "Close"), $listWidget->getCloseUrl()) ?>
-        </li>
-    <?php endif; ?>
-    
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'price')->widget(PriceInput::className()) ?>
