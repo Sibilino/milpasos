ef6bd79a752930170b187d272eee11cbe7739881
Luis Hernández <luishernandez.mail@gmail.com>
2016-08-19 17:52:12 +0200

    Added the content of pass/_listForm

--- views/pass/_listForm.php
+++ views/pass/_listForm.php
@@ -1,1 +1,29 @@
-<?php
+<?php 
+
+use yii\bootstrap\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Pass */
+/* @var $prices app\models\TemporaryPrice[] */
+/* @var $form yii\bootstrap\ActiveForm */
+?>
+
+<h4><?= Html::encode(Html::getAttributeValue($model, 'description')) ?> (<?= Yii::$app->formatter->asCurrency($model->price, $model->currency) ?>)</h4>
+
+<div class="well">
+    
+    <?= $view->render('/pass/_fields', [
+        'model' => $model,
+        'prices' => $prices,
+        'form' => $form,
+    ]) ?>
+    
+    <?= Html::a(Yii::t('app', "Delete"), ['/pass/delete', 'id' => $model->id], [
+        'class' => 'btn btn-danger btn-xs',
+        'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+        'data-method' => 'post',
+        'data-pjax' => 0,
+    ]) ?>
+    
+</div>
+
