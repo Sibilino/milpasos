31919c78f8c4ae22e8d7c0a10cbb277aae4e39b5
Luis Hernández <luishernandez.mail@gmail.com>
2016-09-03 12:22:29 +0200

    Added $events magic property

--- models/forms/EventListForm.php
+++ models/forms/EventListForm.php
@@ -7,6 +7,7 @@ use yii\base\Model;
 
 /**
  * Holds the Ids of the Events selected by the user.
+ * @property Event[] $events Returns the Events selected in this EventListForm.
  * @package app\models\forms
  */
 class EventListForm extends Model
@@ -15,6 +16,11 @@ class EventListForm extends Model
      * @var array|string Array or dash-separated list of ids of the events that the user selected in the map.
      */
     public $eventIds;
+
+    /**
+     * @var Event[]|null Cache of event objects to prevent multiple db aceesses per request.
+     **/
+    private $_events;
     
     public function rules()
     {
@@ -27,5 +33,17 @@ class EventListForm extends Model
             }], // Transform dash-separated string to array
             ['eventIds', 'each', 'rule' => ['exist', 'targetClass'=>Event::className(), 'targetAttribute'=>'id']],
         ];
+    }
+
+    /**
+     * Returns the Events found using the filtering conditions in this MapForm, and caches the results.
+     * @return Event[]
+     **/
+    public function getEvents()
+    {
+        if (!isset($this->_events)) {
+            $this->_events = Event::find()->where(['id'=>$this->eventIds])->all();
+        }
+        return $this->_events;
     }
 }
