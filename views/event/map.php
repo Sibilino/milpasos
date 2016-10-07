7c3a88011cf25969b71fcc1bae869e80e448a983
Luis Hernández <luishernandez.mail@gmail.com>
2016-10-07 14:19:30 +0200

    Added angular to map

--- views/event/map.php
+++ views/event/map.php
@@ -13,6 +13,8 @@ use yii\helpers\Url;
 use yii\web\JsExpression;
 use yii\widgets\ListView;
 
+AngularJsAsset::register($this);
+
 $this->title = 'Milpasos';
 
 $features = array_map(function (Event $e) {
