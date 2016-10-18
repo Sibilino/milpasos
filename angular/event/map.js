bf47c910d79e3003b7b1ac1d9a47dc1d85fea535
Luis Hernández <luishernandez.mail@gmail.com>
2016-10-18 15:30:08 +0200

    Created eventInfo controller

--- angular/event/map.js
+++ angular/event/map.js
@@ -1,1 +1,41 @@
-var eventInfo = angular.module('eventInfo', []);
+(function () {
+    milpasos.mapEventViewer = angular.module('mapEventViewer', []);
+    
+    milpasos.mapEventViewer.controller('eventInfo', function () {
+        this.availableEvents = [];
+        this.selectedEvents = [];
+        this.detailedEvent = null;
+        
+        this.loadEvents = function (events) {
+            this.availableEvents = events;
+            this.selectAll();
+        };
+        
+        this.selectEvents = function (eventIds) {
+            var selection = [];
+            var found = 0;
+            for (var i=0; i<eventIds.length; i++) {
+                for (var j=0; j<this.availableEvents.length; j++) {
+                    if (this.availableEvents[j] == eventIds[i]) {
+                        selection.push(this.availableEvents[j]);
+                        found++;
+                    }
+                }
+            }
+            this.selectedEvents = selection;
+            return found;
+        };
+        
+        this.selectAll = function () {
+            this.selectedEvents = this.availableEvents;
+        };
+        
+        this.openDetails = function (event) {
+            this.detailedEvent = event;
+        };
+        
+        this.closeDetails = function () {
+            this.detailedEvent = null;
+        };
+    });
+})();
