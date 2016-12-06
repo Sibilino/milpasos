451df359b9be8d4db3463631f9a0fca1cb382498
Luis Hernández <luishernandez.mail@gmail.com>
2016-12-06 20:27:28 +0100

    Event selection refactored to service

--- web/js/eventViewer.js
+++ web/js/eventViewer.js
@@ -7,6 +7,24 @@
         };
     });
 
+    app.factory('MapSelector', function () {
+        return {
+            onSelectEvents: function (listener) {
+                milpasos.addMapSelectListener(function (e) {
+                    var eventIds = [];
+                    var features = e.target.getFeatures();
+                    if (features.getLength() > 0) {
+                        features.forEach(function (cluster) {
+                            // Get all features' eventIds
+                            eventIds = cluster.get("features").map(function ($f) {return $f.get("eventId");});
+                        });
+                    }
+                    return listener(eventIds);
+                });
+            }
+        };
+    });
+
     app.config(function($routeProvider) {
         $routeProvider
             .when("/", {
@@ -26,48 +44,40 @@
      * Keeps track of available Events and the subset that has been selected by the user.
      * One of the Events can also be selected for detailed inspection.
      */
-    app.controller('EventManager', ['EventSource', '$scope', function (eventSource, $scope) {
-        $scope.selectedEvents = eventSource.events || [];
+    app.controller('EventManager', ['EventSource', 'MapSelector', '$scope', function (EventSource, MapSelector, $scope) {
+        $scope.selectedEvents = EventSource.events || [];
 
         $scope.selectEvents = function (eventIds) {
             $scope.selectedEvents = [];
             for (var i=0; i<eventIds.length; i++) {
-                for (var j=0; j<eventSource.events.length; j++) {
-                    if (eventSource.events[j].id == eventIds[i]) {
-                        $scope.selectedEvents.push(eventSource.events[j]);
+                for (var j=0; j<EventSource.events.length; j++) {
+                    if (EventSource.events[j].id == eventIds[i]) {
+                        $scope.selectedEvents.push(EventSource.events[j]);
                     }
                 }
             }
         };
 
         $scope.selectAll = function () {
-            $scope.selectedEvents = eventSource.events;
+            $scope.selectedEvents = EventSource.events;
         };
 
-        milpasos.addMapSelectListener(function (e) {
-            // TODO: Refactor this into an injected service that broadcasts event from map listener.
-            var features = e.target.getFeatures();
-            if (features.getLength() == 0) {
-                $scope.$apply(function () {
+        MapSelector.onSelectEvents(function (eventIds) {
+            $scope.$apply(function () {
+                if (eventIds.length === 0) {
                     $scope.selectAll();
-                });
-            } else {
-                features.forEach(function (cluster) {
-                    // Get all features' eventIds
-                    var eventIds = cluster.get("features").map(function ($f) {return $f.get("eventId");});
-                    $scope.$apply(function () {
-                        $scope.selectEvents(eventIds);
-                    });
-                });
-            }
+                } else {
+                    $scope.selectEvents(eventIds);
+                }
+            });
         });
     }]);
 
-    app.controller('DetailView', ['EventSource', '$routeParams', '$scope', function (eventSource, $routeParams, $scope) {
+    app.controller('DetailView', ['EventSource', '$routeParams', '$scope', function (EventSource, $routeParams, $scope) {
         $scope.event = null;
-        for (var i = 0; i<eventSource.events.length; i++) {
-            if (eventSource.events[i].id == $routeParams.id) {
-                $scope.event = eventSource.events[i];
+        for (var i = 0; i<EventSource.events.length; i++) {
+            if (EventSource.events[i].id == $routeParams.id) {
+                $scope.event = EventSource.events[i];
                 break;
             }
         }
