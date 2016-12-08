9746f89b3e73baa3c3aa38a23aee88404e731da0
Luis Hernández <luishernandez.mail@gmail.com>
2016-12-08 20:16:03 +0100

    Added comments

--- web/js/eventViewer.js
+++ web/js/eventViewer.js
@@ -1,14 +1,30 @@
 (function () {
     var app = angular.module('EventViewerApp', ['ngRoute']);
 
+    /**
+     * Service that provides events through the getEvents() method.
+     */
     app.factory('EventSource', function () {
         return {
-            events: milpasos.events || []
+            /**
+             * Returns the Events currently available.
+             * @returns {events|{}|*|Array}
+             */
+            getEvents: function () {
+                return milpasos.events || [];
+            }
         };
     });
 
+    /**
+     * Service that allows assigning handlers to the select event of the Milpasos maps, via addMapSelectListener().
+     */
     app.factory('MapSelector', function () {
         return {
+            /**
+             * The given listener will receive a list of Event ids when the "select" event on the map is fired.
+             * @param listener function(eventIds){} (void return)
+             */
             onSelectEvents: function (listener) {
                 milpasos.addMapSelectListener(function (e) {
                     var eventIds = [];
@@ -24,7 +40,7 @@
             }
         };
     });
-
+    
     app.config(function($routeProvider) {
         $routeProvider
             .when("/", {
@@ -45,21 +61,21 @@
      * One of the Events can also be selected for detailed inspection.
      */
     app.controller('EventManager', ['EventSource', 'MapSelector', '$scope', function (EventSource, MapSelector, $scope) {
-        $scope.selectedEvents = EventSource.events || [];
+        $scope.selectedEvents = EventSource.getEvents() || [];
 
         $scope.selectEvents = function (eventIds) {
             $scope.selectedEvents = [];
             for (var i=0; i<eventIds.length; i++) {
-                for (var j=0; j<EventSource.events.length; j++) {
-                    if (EventSource.events[j].id == eventIds[i]) {
-                        $scope.selectedEvents.push(EventSource.events[j]);
+                for (var j=0; j<EventSource.getEvents().length; j++) {
+                    if (EventSource.getEvents()[j].id == eventIds[i]) {
+                        $scope.selectedEvents.push(EventSource.getEvents()[j]);
                     }
                 }
             }
         };
 
         $scope.selectAll = function () {
-            $scope.selectedEvents = EventSource.events;
+            $scope.selectedEvents = EventSource.getEvents();
         };
 
         MapSelector.onSelectEvents(function (eventIds) {
@@ -75,9 +91,9 @@
 
     app.controller('DetailView', ['EventSource', '$routeParams', '$scope', function (EventSource, $routeParams, $scope) {
         $scope.event = null;
-        for (var i = 0; i<EventSource.events.length; i++) {
-            if (EventSource.events[i].id == $routeParams.id) {
-                $scope.event = EventSource.events[i];
+        for (var i = 0; i<EventSource.getEvents().length; i++) {
+            if (EventSource.getEvents()[i].id == $routeParams.id) {
+                $scope.event = EventSource.getEvents()[i];
                 break;
             }
         }
