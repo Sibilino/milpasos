733734fefe5852222c4961e1de0faf72578e070f
Luis Hernández <luishernandez.mail@gmail.com>
2016-12-09 15:00:25 +0100

    Refactor addMapSelectListener to provide eventIds

--- web/js/mapOptions.js
+++ web/js/mapOptions.js
@@ -23,13 +23,31 @@ sibilino.olwidget.mapOptions["main-map"] = (function ($) {
             ];
         }
     });
-
+    
+    /**
+     * Extracts the Event ids of any Events in the given selection target.
+     * @return Array of integers
+     */
+    var extractEventIds = function (target) {
+        var eventIds = [];
+        var features = target.getFeatures();
+        if (features.getLength() > 0) {
+            features.forEach(function (cluster) {
+                // Get all features' eventIds
+                eventIds = cluster.get("features").map(function ($f) {return $f.get("eventId");});
+            });
+        }
+        return eventIds;
+    }
+    
     /**
      * Publish "select" event of the map's Select interaction into a public module.
      * @param listener
      */
     milpasos.addMapSelectListener = function (listener) {
-        select.on("select", listener);
+        select.on("select", function (e) {
+            listener(extractEventIds(e.target));
+        });
     };
     
     return {
