b9a08e871b8342ae9f853ac88e378a6430744898
Luis Hernández <luishernandez.mail@gmail.com>
2016-12-27 20:59:28 +0100

    Implemented API group name search

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -1,8 +1,14 @@
-use yii\rest\Controller;
+<?php
 
 namespace app\controllers;
 
+use app\models\Group;
+use yii\rest\Controller;
+
 class ApiController extends Controller
 {
-
+    public function actionGroupSearch($term = '')
+    {
+        return Group::find()->where(['like', 'name', $term])->all();
+    }
 }
