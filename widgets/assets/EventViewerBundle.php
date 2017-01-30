efd147eaf57a5e5ae913b7536652b1d7332bbc4e
Luis Hernández <luishernandez.mail@gmail.com>
2017-01-30 20:40:04 +0100

    Evenviewerbundle now uses base angularbundle

--- widgets/assets/EventViewerBundle.php
+++ widgets/assets/EventViewerBundle.php
@@ -2,16 +2,12 @@
 
 namespace app\widgets\assets;
 
-use app\assets\AngularJsAsset;
-use yii\web\AssetBundle;
-
 /**
  * Assets for the EventViewer widget.
  * @package app\widgets\assets
  */
-class EventViewerBundle extends AssetBundle
+class EventViewerBundle extends AngularBundle
 {
-    public $baseUrl = '@web/js';
     public $depends = [
         'app\assets\AngularJsAsset',
         'app\assets\MilpasosAsset',
@@ -19,8 +15,4 @@ class EventViewerBundle extends AssetBundle
     public $js = [
         'eventViewer.js',
     ];
-    
-    public static function GetAngularAppName() {
-        return AngularJsAsset::ANGULAR_APP_NAME;
-    }
 }
